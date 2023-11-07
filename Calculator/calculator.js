c660fac87fed4676b255cdde5956ffe54af2cd34
michaelr3207 <michael--robinson321@hotmail.com>
2023-11-07 03:53:21 +0000

    ssecirst commit, comment added

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -3,7 +3,7 @@ console.log('testtsstst');
 
 // this is a test comment
 // this is a nother test commit
-//this is a third commit
+//this is a third commit again
 
 function inputNumber1(number){
     const output = document.getElementById("output");
